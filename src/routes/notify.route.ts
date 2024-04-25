ff7f752386a331f2076e7a26adc3349c53145679
phamdung2209 <phamdung.22092003@gmail.com>
2024-04-25 09:57:34 +0700

    Update HTTP method for notify route from GET to POST

--- src/routes/notify.route.ts
+++ src/routes/notify.route.ts
@@ -3,7 +3,7 @@ import { getNotify } from '../controllers/notify.controller'
 
 const router = express.Router()
 
-router.get('/', getNotify)
+router.post('/', getNotify)
 // router.post('/')
 
 export default router
