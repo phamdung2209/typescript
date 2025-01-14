a27eb0993f1090a56b1f45b22001b1cd233f4b32
PHAM VAN DUNG <119159181+phamdung2209@users.noreply.github.com>
2025-01-14 05:42:55 +0000

    test build

--- src/controllers/instagram.controller.ts
+++ src/controllers/instagram.controller.ts
@@ -2,11 +2,13 @@ import { Request, Response } from "express";
 
 export const getWebhook = async (req: Request, res: Response) => {
     try {
-        console.log('res', res)
+        const hub = req.query
 
         res.json({
             message: 'Get from webhook instagram'
         })
+
+        res.json(hub['hub.challenge'])
     } catch (error: any) {
         console.log('Error in getWebhook controller', error.message)
         res.json({ error: 'Internal server error' })
