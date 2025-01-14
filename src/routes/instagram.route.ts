e3806518097c2a1b0fa12e3ecbe079509759f138
PHAM VAN DUNG <119159181+phamdung2209@users.noreply.github.com>
2025-01-14 11:39:06 +0700

    .

--- src/routes/instagram.route.ts
+++ src/routes/instagram.route.ts
@@ -1,5 +1,5 @@
 import express from 'express'
-import { getWebhook } from '~/controllers/instagram.controller'
+import { getWebhook } from '../controllers/instagram.controller'
 
 const router = express.Router()
 
